cb024bda0feb808f56a6cdf22be725d7f554dfc0
CoffeeLink <idangws14@gmail.com>
2020-12-04 09:24:56 +0700

    changes for testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const title = 'App Counter';
+  let [number, setNumber] = useState(0)
+
+  const incrementNumber = () => {
+    setNumber((prevNumb) => prevNumb + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div data-test="component-app">
+      <h1>{ title }</h1>
+      <br />
+      number clicked { number }
+      <br />
+      <button onClick={incrementNumber}>Click</button>
     </div>
   );
 }
